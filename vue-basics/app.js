d5d33da1f19edcd22d1b46c85b0ca5ccfe693620
devsakaso <pg165m@gmail.com>
2021-03-02 22:28:49 +0900

    mouse event

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -6,18 +6,33 @@ const app = Vue.createApp({
       author: "著者名",
       age: 32,
       showBooks: true,
+      x: 0, // mousemove用
+      y: 0, // mousemove用
     };
   },
   methods: {
     // 単純なクリックイベントの関数
     changeTitle(title) {
-      this.title = title
+      this.title = title;
     },
     // トグルしたい場合、リバースするために!をつける
     toggleShowBooks() {
-      this.showBooks = !this.showBooks
-    }
-  }
+      this.showBooks = !this.showBooks;
+    },
+    // 第一引数にevent情報、第二引数にオプショナルなデータを受け取る
+    // eにevent情報が格納されている
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      // offsetはeの情報をコンソールで確認できる
+      this.x = e.offsetX
+      this.y = e.offsetY
+    },
+  },
 });
 
 // mountで指定した範囲内でしかvueは動かない
