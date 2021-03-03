246a5529f2948c01d7c30e2c0f71d708d15fa6ec
devsakaso <pg165m@gmail.com>
2021-03-04 03:27:24 +0900

    Add emit and event Modifier

--- vue-cli/modal-project/src/App.vue
+++ vue-cli/modal-project/src/App.vue
@@ -1,23 +1,43 @@
 <template>
+  <!-- App.vue root component -->
   <h1>{{ title }}</h1>
-  <hr>
-  <input type="text" ref="name">
+  <div v-if="showModal">
+    <!-- // 親コンポーネントでは、closeが発動したらどうしたいのかを書く。
+    今回の場合modalを閉じたいのでtoggleModalを渡す -->
+    <!-- イベントが伝搬しているので背景をクリックしてもその子要素をクリックしてもmodalが反応するので、Event Modifierを設定する -->
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <!-- Event Modifierの設定方法
+  @clickの後に.rightの形で続ける。
+  .rightは右クリックしたら、.shiftはshiftキープラスクリックしたら -->
+  <button @click.shift="toggleModal">Open Modal(shift)</button>
+  <hr />
+  <input type="text" ref="name" />
   <button @click="handleClick">click me</button>
 </template>
 
 <script>
+import Modal from './components/Modal';
+
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'My First Vue App'
+      title: 'My First Vue App',
+      header: 'ヘッダー部分',
+      text: 'ここにテキスト',
+      showModal: false
     };
   },
   methods: {
     handleClick(e) {
-      console.log(this.$refs.name);//ref="name"の入っている<input type="text">が出力される
-      this.$refs.name.classList.add('active') //activeクラスを付与する
-      this.$refs.name.focus() //clickしたらref="name"にfocusする
+      console.log(this.$refs.name); //ref="name"の入っている<input type="text">が出力される
+      this.$refs.name.classList.add('active'); //activeクラスを付与する
+      this.$refs.name.focus(); //clickしたらref="name"にfocusする
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
     }
   }
 };
---
+++ vue-cli/modal-project/src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+<!-- Event Modifierの設定方法
+@clickのあとに.selfとするとその要素のみにイベントが適用される -->
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale'}">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 親から値を受け取るprops
+  props:['header', 'text', 'theme'],
+  methods: {
+    closeModal() {
+      // emitの中に好きな名前をつけることで親コンポーネントでclickイベントのようなイベントのように使うことができる
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background-color: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background-color: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+  h1 {
+    color: #02cfb4;
+    border:none;
+    padding: 0;
+  }
+  .modal.sale {
+    background-color: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+</style>
