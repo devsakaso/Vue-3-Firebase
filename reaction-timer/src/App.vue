6fc43cfc9a04cf55f3dbd626f10c6617ab092819
devsakaso <pg165m@gmail.com>
2021-03-04 21:09:25 +0900

    Complete Timer

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <!-- v-bind:disabled="isPlaying"はtrueのときdisabledにする -->
+  <button @click="start" v-bind:disabled="isPlaying">スタート</button>
+  <Block v-if="isPlaying" v-bind:delay="delay" @end="endGame" />
+  <Results v-if="showResults" v-bind:score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Block.vueを使うには、
+// 1. importして
+// 2. componentsにBlockを加えて
+// 3. templateタグでBlockタグを使う
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null, //reactionTimeを受け取りたいので格納する変数を作る
+      showResults: false
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 3000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    // Gameを終わらせる
+    // $emitの第二引数で渡される値を引数で受け取る
+    // reactionTimeを表示するのと、isPlayingをfalseに戻す
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -20,7 +49,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button {
+  background-color: #31b0c7;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 18px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- 
+  App.vueのscoreを使うためには、
+  App.vueのResultsタグでv-bind:score="score"としてResultsにわたす.
+  Results.vueでpropsに追加する
+  templateタグで使う
+ -->
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    };
+  },
+  mounted() {
+    if (this.score < 260) {
+      this.rank = '超一流';
+    } else if (this.score < 400) {
+      this.rank = '一流';
+    } else {
+      this.rank = '二流';
+    }
+  }
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.8em;
+  color: #31b0c7;
+  font-weight: bold;
+}
+</style>
